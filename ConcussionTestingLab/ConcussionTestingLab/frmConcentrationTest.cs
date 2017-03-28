468e3101f0cb83958f4431c862a8e762a75c9168
charbin92 <charbin@murraystate.edu>
2017-03-28 10:39:02 -0500

    Added New Forms
    Created forms for each of the test and created a User Form That will
    display the user Data and allow the user to select which test they would
    like to take.

--- ConcussionTestingLab/ConcussionTestingLab/frmConcentrationTest.cs
+++ ConcussionTestingLab/ConcussionTestingLab/frmConcentrationTest.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ConcussionTestingLab
 {
-    public partial class Form1 : Form
+    public partial class frmConcentrationTest : Form
     {
-        public Form1()
+        public frmConcentrationTest()
         {
             InitializeComponent();
         }
--- ConcussionTestingLab/ConcussionTestingLab/frmReactionTimeTest.Designer.cs
+++ ConcussionTestingLab/ConcussionTestingLab/frmReactionTimeTest.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ConcussionTestingLab
 {
-    partial class Form1
+    partial class frmReactionTimeTest
     {
         /// <summary>
         /// Required designer variable.
@@ -30,10 +30,9 @@
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.Text = "frmReactionTimeTest";
         }
 
         #endregion
     }
 }
-
